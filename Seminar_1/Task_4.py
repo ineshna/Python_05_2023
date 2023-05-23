243482a6032caf3d026ac942dfa8a55fd7cda5a6
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-05-23 15:16:12 +0300

    task2

--- Seminar_1/Task_4.py
+++ Seminar_1/Task_4.py
@@ -2,4 +2,11 @@
 #Требуется определить, является ли год с данным номером високосным
 year = int(input("Введите год :"))
 
-print('Yes') if ((year % 4 ==0) and (year % 400 == 0) or (year % 400 == 0)) else print ('No')
+# print('Yes') if year % 4 ==0 and year % 400 == 0 or year % 400 == 0 else print ('No')
+
+condition1 = year % 4 == 0 and year % 100 != 0
+condition2 = year % 400 == 0
+if condition1 or condition2:
+    print ('Yes')
+else:
+    print("No")

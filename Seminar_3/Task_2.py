5e7c4eb7b5791a2d172274b6a807e9bd9e49cc89
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-05-29 13:43:12 +0300

    3_task_2

--- Seminar_3/Task_2.py
+++ Seminar_3/Task_2.py
@@ -8,4 +8,8 @@
 #Input: [1, 2, 3, 4, 5] k = 3
 #Output: [4, 5, 1, 2, 3]
 list_1 = [1, 2, 3, 4, 5]
-k = int(input('Введите число'))
+k = int(input('Введите число :'))
+resul_list =[]
+resul_list = list_1[k:] + list_1[:k]
+print(list_1)
+print(resul_list)

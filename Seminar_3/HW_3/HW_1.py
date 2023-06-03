65d604fe3a54df21da4e2440fc88fe4ad1f340f5
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-06-03 23:34:10 +0300

    HW3

--- Seminar_3/HW_3/HW_1.py
+++ Seminar_3/HW_3/HW_1.py
@@ -17,6 +17,11 @@
 list_1 = [10, 5, 7, 3, 3, 0, 5, 7, 2, 8]
 x = int(input('Введите искомое число :'))
 result = 0
+my_count = list_1.count(x)
+if my_count == 0:
+ print("-1")
+else:
+ print(my_count)
 """
 for i in list_1:
        if x not in list_1:
@@ -31,5 +36,5 @@ for i in list_1:
       result = -1
 print(result)
 """
-print(list_1.count(x))
+#print(list_1.count(x))
 
--- Seminar_3/HW_3/HW_2.py
+++ Seminar_3/HW_3/HW_2.py
@@ -8,7 +8,17 @@
     Output: 2
     Input: [10, 5, 7, 3, 3, 2, 5, 7, 3, 8], X = 9
 """
-list_1 = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
-x = int(input('Введите число:'))
+# list_1 = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
+# x = int(input('Введите число:'))
 #print(min(list_1, key=lambda a:abs(a-x)))
-b=[abs(list_1[i]-x) for i in range(len(list_1))]
+#b=[abs(list_1[i]-x) for i in range(len(list_1))]
+list_1 = [1,3,6,3,5,67,2,2,4,89,-9,3]
+x = int(input("Введите число X: "))
+
+min_diff = abs(x - list_1[0]) #считаем для 0го элемента расстояние до Х
+index_min_diff = 0
+for num in range(1,len(list_1)):
+ if min_diff > abs(x-list_1[num]):
+     min_diff = abs(x-list_1[num])
+     index_min_diff = num
+print(f'На позиции {index_min_diff} в списке находится самый близкий к {x} элемент и он равен {list_1[index_min_diff]}')

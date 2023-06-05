d385720361faff94bfdc694d644bc37306960edb
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-06-05 14:50:37 +0300

    task_1

---
+++ Seminar_4/4Task_1.py
@@ -0,0 +1,39 @@
+# Напишите программу, которая принимает на вход строку, и отслеживает, 
+# сколько раз каждый символ уже встречался.
+# Количество повторов добавляется к символам с помощью постфикса формата _n.
+"""
+Примеры/Тесты:
+
+Input: a a a b c a a d c d d
+Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+
+Input: a b c d a a a a a a a
+Output: a b c d a_1 a_2 a_3 a_4 a_5 a_6 a_7
+
+Строку не обязательно вводить с клавиатуры
+"""
+list_1 = "a a a b c a a d c d d"
+result =[]
+for ind, val in enumerate(list_1):
+    n = list_1[0:ind].count(val)
+    if n > 0:
+        result.append(f"{val}_{n}")
+    else:
+        result.append(val)
+print("".join(result))
+
+# тернарный оператор вокруг addend
+# list_1 = "a a a b c a a d c d d"
+# result =[]
+# for ind, val in enumerate(list_1):
+#     n = list_1[0:ind].count(val)
+#     result.append(f"{val}_{n}") if n > 0 else result.append(val)
+# print("".join(result))
+
+# тернарный оператор вокруг значения
+# list_1 = "a a a b c a a d c d d"
+# result =[]
+# for ind, val in enumerate(list_1):
+#     n = list_1[0:ind].count(val)
+#     result.append(f"{vav}_{n}" if n > 0 else val)
+# print("".join(result))

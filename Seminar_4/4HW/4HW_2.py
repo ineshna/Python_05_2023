c5077ed1249ddf63bc9584a1070d5e693576d556
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-06-06 00:09:11 +0300

    HW_2

--- Seminar_4/4HW/4HW_2.py
+++ Seminar_4/4HW/4HW_2.py
@@ -14,4 +14,15 @@
 
     Input1: 11, 92, 1, 42, 15, 12, 11, 81
     Output1: Макс. кол-во ягод 184, собрано для куста 1
+    
 """
+n = int(input())
+arr = list()
+for i in range (n):
+    x = int(input())
+    arr.append(x)
+arr_count = list()
+for i in range(len(arr - 1)):
+    arr_count.append(arr[i -1 ] + arr[i] + arr[i+1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
